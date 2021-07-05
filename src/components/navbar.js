edac15ed6e513e560d2e10c9cc56777c7f1720b1
Miftakhunnurudin <Miftakhunnurudin@gmail.com>
2021-07-05 22:51:22 +0700

    starting the navbar

---
+++ src/components/navbar.js
@@ -0,0 +1,12 @@
+
+function Navbar () {
+    return (
+        <>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                a
+            </nav>
+        </>
+    )
+}
+
+export default Navbar
